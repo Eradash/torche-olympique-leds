3d4c424b90a59404117d4525dd57aea6b86e7e37
Vincent Poirier <poirier.20.100@hotmail.com>
2018-10-23 13:45:19 -0400

    Change for a Fritzing project

--- code.ino
+++ code.ino
@@ -1,11 +1,10 @@
 
-
 int maximum = 255; //Valeur maximum des leds (MAX: 255)
 int minimum = 100; //Valeur minimum des leds (MIN: 0)
 
-int changeSpeed = 100; //DÃ©lais d'attente en ms
+int changeSpeed = 100; //Délais d'attente en ms
 
-int leds[] = {1, 2, 3}; //Mettre tous les numÃ©ros de pin pour les leds ici
+int leds[] = {3, 5, 9, 11}; //Mettre tous les numéros de pin pour les leds ici
 
 void setup() {
   for(int led : leds)
